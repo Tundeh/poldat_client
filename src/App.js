025c8dd73323aade52eafb0a7afd34341ea6255d
Tundeh <jamiu4freedom@gmail.com>
2020-08-06 20:09:35 +0100

    modification for development setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHome,
+  faUser,
+  faAddressbook,
+} from "@fortawesome/free-brands-svg-icons";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+library.add(faHome, faUser, faAddressbook);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
