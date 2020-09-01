9081253e3a5d35de83fb81f69073b2c290c7d0ee
Tundeh <jamiu4freedom@gmail.com>
2020-09-01 13:44:35 +0100

    add footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss'
+
+
+const Footer = (props) => {
+    return (
+        <div className="footer">
+            <span>C @ 2020 Tundeh designs</span>
+        </div>
+    );
+};
+
+export default Footer;
