cb28cd5b1c6f608a479f22f9072f4a58d951de47
Tundeh <jamiu4freedom@gmail.com>
2020-10-09 16:52:26 +0100

    change signup actions to logout actions

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -49,6 +49,6 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
 //SignUp Action Types
-export const LOGOUT_REQUEST = "SIGNUP_REQUEST";
-export const LOGOUT_SUCCESS = "SIGNUP_SUCCESS";
-export const LOGOUT_FAILURE = "SIGNUP_FAILURE";
+export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const LOGOUT_FAILURE = "LOGOUT_FAILURE";
