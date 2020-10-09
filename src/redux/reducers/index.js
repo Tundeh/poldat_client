83e10fb06344661b42f059c842c0d01af89e0ae6
Tundeh <jamiu4freedom@gmail.com>
2020-10-09 09:03:36 +0100

    import members, users, groups to combine reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,14 @@
 import {combineReducers} from "redux";
 import navigationReducer from "./navigationReducer";
+import members from "./memberReducer";
+import users from "./userReducer";
+import groups from "./groupReducer";
 
 const rootReducer = combineReducers({
-  navigationReducer
+  navigationReducer,
+  members,
+  users,
+  groups
 })
 
 export default rootReducer;
