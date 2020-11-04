74465a1eadf8a0e1ec634eb098f3959cec1c5751
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 14:32:27 +0100

    include load group request, success and failure

--- src/redux/actions/groupActions.js
+++ src/redux/actions/groupActions.js
@@ -1,13 +1,44 @@
 import * as actionTypes from "./actionTypes";
 import * as groupApi from "../../api/groupApi";
 
-export const loadGroupsSuccess = (user) => {
-  return {type: actionTypes.SAVE_GROUP_SUCCESS, user }
+export const loadGroupsRequest = () => {
+  return {type: actionTypes.LOAD_GROUP_REQUEST}
+}
+
+export const loadGroupsSuccess = (groups) => {
+  return {type: actionTypes.LOAD_GROUP_SUCCESS, groups }
+}
+
+export const loadGroupsFailure = (message) => {
+  return {type: actionTypes.LOAD_GROUP_FAILURE, message}
+}
+
+export const saveGroupRequest = () => {
+  return {type: actionTypes.SAVE_GROUP_REQUEST}
+}
+
+export const saveGroupSuccess = (message) => {
+  return {type: actionTypes.SAVE_GROUP_SUCCESS, message}
+}
+
+export const saveGroupFailure = (message) => {
+  return {type: actionTypes.SAVE_GROUP_FAILURE, message}
 }
 
 
-export function loadGroups(users) {
+export function loadGroups() {
   return function(dispatch) {
-    return groupApi.getGroups().then(groups => dispatch(loadGroupsSuccess(groups)))
+    dispatch(loadGroupsRequest())
+    return groupApi.getGroups().then(groups => dispatch(loadGroupsSuccess(groups))).catch(error => {
+      loadGroupsFailure(error)
+    })
+  }
+}
+export function saveGroup(group) {
+  return function(dispatch) {
+    dispatch(saveGroupRequest())
+    return groupApi.saveGroup(group).then(saveGroupSuccess("group successfully saved")).catch(
+      err => saveGroupFailure("Group saving fail, try again!")
+    )
   }
 }
