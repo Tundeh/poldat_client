e7b78fbd4f566c8b155669734100e95ab8f76d89
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 14:25:07 +0100

    add group reducers cases

--- src/redux/reducers/groupReducer.js
+++ src/redux/reducers/groupReducer.js
@@ -3,8 +3,24 @@ import initialState from "./initialState";
 
 export default function groupReducer(state=initialState.groups, action) {
   switch(action.type){
+    case actionTypes.LOAD_GROUP_REQUEST:
+
+      return {...state, isFetching: true}
+      
     case actionTypes.LOAD_GROUP_SUCCESS: 
-    return action.groups
+
+      return {...state, isFetching: false, groups: action.groups}
+
+    case actionTypes.LOAD_GROUP_FAILURE: 
+
+      return {...state, isFetching: false, message: action.message}
+
+    case actionTypes.SAVE_GROUP_REQUEST:
+      return {...state, isSaving: true, message: action.message}
+    case actionTypes.SAVE_GROUP_SUCCESS: 
+      return {...state, isSaving: false, message: action.message}
+    case actionTypes.SAVE_GROUP_FAILURE:
+      return {...state, isSaving: false, message: action.message}
     default:
       return state
   }
