fe80bda63e3c695c21b35b47ba04d4ccd95a91db
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 14:28:41 +0100

    initialize member object

--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,6 +1,9 @@
 export default {
   collapse_menu: false,
-  members: [],
+  members: {members: [], member: {}},
   users: [],
-  groups: []
+  groups: {groups: []},
+  auth: {
+    isAuthenticated: false
+  }
 }
