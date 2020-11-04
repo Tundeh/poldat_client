ce337d521239200d7d08c930892d73800f7a8352
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 14:42:25 +0100

    include response is 401

--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -1,12 +1,31 @@
 export const handleResponse = async (response) => {
-  if (response.ok) return response.json();
+  
+  if (response.ok) {
+    
+    return await response.json();
+  }
+
   if (response.status === 400) {
+   
     const error = await response.text();
     throw new Error(error);
   }
-  return new Error("Network response not ok");
+  
+  if(response.status === 401){
+    throw new Error("Not Authorized");
+  }
+  
+   throw new Error("Network response not ok");
 };
 
 export const handleError = (error) => {
- return error;
+  console.log(error)
+  throw error;
 };
+
+export const authHeader = () => {
+  return {
+    "content-type": "application/json",
+    "Authorization": "Bearer " + localStorage.getItem("token")
+  }
+}
