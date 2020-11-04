c8b53f17a74a7f2b84939ac62957057b0b002242
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 15:48:44 +0100

    include load member functionality

--- src/redux/actions/memberActions.js
+++ src/redux/actions/memberActions.js
@@ -1,18 +1,51 @@
 import * as membersApi from "../../api/membersApi";
 import * as actionTypes from "./actionTypes";
-import {apiCallError}  from "./apiStatusActions";
 
-export function loadMemberSuccess(members) {
+export function loadMembersSuccess(members) {
   return {type: actionTypes.LOAD_MEMBERS, members}
 }
+
 export function saveMemberSuccess(member) {
   return{type: actionTypes.SAVE_MEMBER_SUCCESS, member}
+}
+
+
+export function saveMemberRequest() {
+  return {type: actionTypes.SAVE_MEMBER_REQUEST}
+}
+
+export function saveMemberFailure(message) {
+  return {type: actionTypes.SAVE_MEMBER_FAILURE, message}
+}
+
+export function deleteMemberRequest(){
+  return {type: actionTypes.DELETE_MEMBER_REQUEST}
+}
+
+export function deleteMemberSuccess(message) {
+  return {type: actionTypes.DELETE_MEMBER_SUCCESS, message}
+}
+
+export function deleteMemberFailure(message) {
+  return {type: actionTypes.DELETE_MEMBER_FAILURE, message}
+}
+
+export function loadMemberRequest(){
+  return {type: actionTypes.LOAD_MEMBER_REQUEST}
+}
+
+export function loadMemberSuccess(member){
+  return {type: actionTypes.LOAD_MEMBER_SUCCESS, member}
+}
+
+export function loadMemberFailure(message){
+  return {type: actionTypes.LOAD_MEMBER_FAILURE, message}
 }
 
 export function loadMembers() {
   return function(dispatch){
     return  membersApi.getMembers().then(members => {
-    dispatch(loadMemberSuccess(members));
+    dispatch(loadMembersSuccess(members));
   }).catch(error => {
     console.log(error);
   })
@@ -22,8 +55,23 @@ export function loadMembers() {
 
 export function SaveMember(member) {
   return function(dispatch) {
+    dispatch(saveMemberRequest())
     return membersApi.saveMember(member).then(savedMember => {
-      dispatch(saveMemberSuccess(savedMember))
-    })
+      dispatch(saveMemberSuccess("saved successfully"))
+    }).catch(errors => dispatch(saveMemberFailure(errors)))
+  }
+}
+
+export function deleteMember(member){
+  return function(dispatch){
+    dispatch(deleteMemberRequest())
+    return membersApi.deleteMember(member).then(dispatch(deleteMemberSuccess("Member deleted successfully"))).catch(deleteMemberFailure("Member not deleted"))
+  }
+}
+
+export function loadMember(id) {
+  return function(dispatch){
+    dispatch(loadMemberRequest())
+    return membersApi.getMember(id).then(member => dispatch(loadMemberSuccess(member))).catch(dispatch(loadMemberFailure("Unable to get member details")))
   }
 }
