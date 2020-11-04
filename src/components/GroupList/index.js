5095ea36642ac0d9c4abca1337949a4fae0bd945
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 14:39:35 +0100

    add proptypes

--- src/components/GroupList/index.js
+++ src/components/GroupList/index.js
@@ -1,16 +1,20 @@
 import React, {useEffect} from "react";
 import { Container, Table } from "react-bootstrap";
+import  propTypes  from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+
 import * as groupActions from "../../redux/actions/groupActions";
 
 import "./index.scss";
 
 const GroupList = ({groups, actions}) => {
+
   useEffect(() => {
-    actions.loadGroups();
+    actions.loadGroups();;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
   return (
     <Container>
        <div className="form_header"><h2>Groups List</h2></div>
@@ -21,10 +25,8 @@ const GroupList = ({groups, actions}) => {
           <th>Group Leader</th>
           {groups.map(group => {
            return ( <tr key={group.__id}>
-              <td></td>
-              <td>group.group_name</td>
-              <td>group.group_leader</td>
-              <td>group.date_created</td>
+              <td>{group._id}</td>
+              <td>{group.name}</td>
             </tr>)
           })}
         </tbody>
@@ -33,18 +35,23 @@ const GroupList = ({groups, actions}) => {
   );
 };
 
+GroupList.propTypes = {
+  groups: propTypes.object.isRequired,
+  actions: propTypes.object.isRequired
+}
+
 const mapStateToProps = (state) => {
   return {
-    groups: state.groups,
+    groups: state.groups.groups,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-  actions: {
+  return { actions: {
     loadGroups: bindActionCreators(groupActions.loadGroups, dispatch)  
   }
   };
 };
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(GroupList);
