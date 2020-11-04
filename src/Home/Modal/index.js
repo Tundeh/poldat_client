35849b8760b672f4fb94a1ed631699a845cac398
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 15:47:23 +0100

    add input validation with yup

--- src/Home/Modal/index.js
+++ src/Home/Modal/index.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Spinner } from "react-bootstrap";
+import {Formik} from "formik";
+import propTypes from "prop-types";
+import * as yup from "yup";
+
 import "./index.scss";
-const Modal = ({isShowing, handleClick}) => {
-  
+const Modal = ({isShowing, handleClick, handleLogin, auth}) => {
+    const schema = yup.object({
+      email: yup.string().email().required(),
+      password: yup.string().required()
+    })
 return(
   isShowing ?
   <div className="login__modal">
@@ -15,27 +22,52 @@ return(
         </div>
         
     <div>
-    <Form >
+      <Formik
+      validationSchema={schema}
+      onSubmit={values => handleLogin(values)}
+      initialValues={{
+        email: "",
+        password: ""
+      }}
+      > 
+        {({errors, getFieldProps, handleSubmit, touched}) => (
+    <Form onSubmit={handleSubmit}>
             <Form.Group>
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" required/>
+              <Form.Control name="email" type="email" 
+              {...getFieldProps("email")}
+              isInvalid={touched.email && errors.email}
+              />
+              <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
                 
             </Form.Group>
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control required/>
+              <Form.Control name="password" type="password" {...getFieldProps("password")}
+              isInvalid={touched.password && errors.password}
+              />
+              <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
             </Form.Group>
 
-            <Form.Group>
-              <Button type="submit">Log In</Button>
+            <Form.Group style={{display: "flex", justifyContent: "center"}}>
+              <Button className="submit__button" type="submit">Log In {auth.isFetching ? <Spinner animation="border" variant="secondary" /> : null}</Button> 
             </Form.Group>
         </Form>
+        )}
+        </Formik>
     </div>
     
     </div>
   </div>
   
-: null)
+: null
+)
 }
 
+Modal.propTypes = {
+  handleLogin: propTypes.func.isRequired,
+  auth: propTypes.object.isRequired,
+  isShowing: propTypes.bool.isRequired,
+  handleClick: propTypes.func.isRequired
+}
 export default Modal;
