2a4960325fc2ac3b0b31530736be4ed60df337c7
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 14:27:36 +0100

    include some cases to member reducer

--- src/redux/reducers/memberReducer.js
+++ src/redux/reducers/memberReducer.js
@@ -4,9 +4,25 @@ import initialState from "./initialState";
 export default function memberReducer(state = initialState.members, action){
   switch (action.type) {
     case actionTypes.LOAD_MEMBERS:
-      return [...state, ...action.members];
+      return {...state, members: action.members};
     case actionTypes.SAVE_MEMBER_SUCCESS:
-      return [...state, {...action.member}];
+      return {...state, isSaving: false, message: action.message}
+    case actionTypes.SAVE_MEMBER_REQUEST:
+      return {...state, isSaving: true}
+    case actionTypes.SAVE_MEMBER_FAILURE:
+      return {...state, isSaving: false, message: action.message}
+    case actionTypes.DELETE_MEMBER_REQUEST: 
+      return {...state, isDeleting: true}
+    case actionTypes.DELETE_MEMBER_SUCCESS:
+      return {...state, isDeleting: false, message: action.message}
+    case actionTypes.DELETE_MEMBER_FAILURE:
+      return {...state, members: (state.members.filter(member => member.id !== action.member.id)), isDeleting: false, message: action.message}
+    case actionTypes.LOAD_MEMBER_REQUEST:
+      return {...state, isFetching: true}
+    case actionTypes.LOAD_MEMBER_SUCCESS:
+      return {...state, isFetching: false, member: action.member}
+    case actionTypes.LOAD_MEMBER_FAILURE: 
+      return {...state, isFetching: false, message: action.message}
     default: 
       return state;
   }
