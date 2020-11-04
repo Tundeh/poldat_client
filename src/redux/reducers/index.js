c72fa0a70ab6107565f795e4d677ffc6e2cd818d
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 15:50:25 +0100

    include authentication reducer in root reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import navigationReducer from "./navigationReducer";
 import members from "./memberReducer";
 import users from "./userReducer";
 import groups from "./groupReducer";
+import auth from "./authReducer";
 
 const rootReducer = combineReducers({
   navigationReducer,
   members,
   users,
-  groups
+  groups,
+  auth
 })
 
 export default rootReducer;
