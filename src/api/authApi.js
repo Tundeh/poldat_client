38b933c320f669e1a59191afc17aa99df3722ddc
Tundeh <jamiu4freedom@gmail.com>
2020-11-04 07:53:20 +0100

    add auth functions

---
+++ src/api/authApi.js
@@ -0,0 +1,19 @@
+import userEvent from "@testing-library/user-event";
+import { handleError, handleResponse } from "./apiUtils";
+
+const keys = require("../config/keys");
+const baseUrl = keys.apiURL + "auth/login/";
+
+export const login = (userInfo) => {
+  console.log(JSON.stringify(userInfo));
+   return fetch(baseUrl, {
+     method: "POST",
+     headers: {
+       "content-type": "application/json"
+     },
+    body: JSON.stringify(userInfo)
+   }).then(handleResponse).catch(handleError);
+}
+export const logout = (userInfo) => {
+  
+}
